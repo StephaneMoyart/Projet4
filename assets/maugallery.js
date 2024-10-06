34dfb3b1137d6ae80a823bfeb4bf820828d43247
stephaneM <moyartstephane@gmail.com>
2024-10-06 18:37:59 +0200

    add upgrades

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -154,7 +154,7 @@
         }
       });
       next =
-        imagesCollection[index] ||
+        imagesCollection[index - 1] ||
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -192,7 +192,8 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      console.log('imagesCollection'[index])
+      next = imagesCollection[index + 1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
@@ -240,7 +241,7 @@
         return;
       }
       $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active-tag");
+      $(this).addClass("active active-tag");
 
       var tag = $(this).data("images-toggle");
 
